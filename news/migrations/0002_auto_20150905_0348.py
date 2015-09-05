c6179aca1bf264e311d221df6c7cdb43c22fd83b
anduo <anduo@meituan.com>
2015-09-05 11:55:07 +0800

    create project and app

---
+++ news/migrations/0002_auto_20150905_0348.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('news', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='article',
+            name='create_time',
+            field=models.DateTimeField(default=datetime.datetime(2015, 9, 5, 3, 48, 3, 264522, tzinfo=utc), verbose_name='\u53d1\u8868\u65f6\u95f4', auto_now_add=True),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='update_time',
+            field=models.DateTimeField(auto_now=True, verbose_name='\u66f4\u65b0\u65f6\u95f4', null=True),
+        ),
+    ]
